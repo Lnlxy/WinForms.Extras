5898a48543dbe3a4006111f571e53497fb4919fa
Hao Liu <hoze@live.cn>
2017-11-10 15:44:35 +0800

    提交Nuget
    提交打包描述

--- WinForms.Extras/Properties/AssemblyInfo.cs
+++ WinForms.Extras/Properties/AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("WinForms.Extras")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This project is an extension of the WinForms development function, enabling the use of data binding and command binding in WinForms program development")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("hoze")]
 [assembly: AssemblyProduct("WinForms.Extras")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
